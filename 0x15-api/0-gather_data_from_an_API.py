bd90713868b79072d1d129c9cd1d385aca4c7f3b
MutaiAllan <allankiprop175@gmail.com>
2023-05-25 10:44:05 -0700

    Solved task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Returns information about a given employee's TODO list progress.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    restapi = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(restapi + "users/{}".format(sys.argv[1])).json()
+    todores = requests.get(restapi + "todos", params={"userId": sys.argv[1]})
+    todo = todores.json()
+
+    cdone = 0
+    done = []
+
+    for task in todo:
+        if task.get('completed'):
+            done.append(task)
+            cdone += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user, cdone, len(todo)))
+
+    for task in done:
+        print("\t {}".format(task.get('title')))
